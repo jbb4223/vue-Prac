150f701a8686e1b091c025132a4ce5e995c48afd
jbb4223 <jbb4223_3top@gsretail.com>
2023-05-17 16:28:26 +0900

    10 class

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <input
-      type="text"
-      v-model="Name"
-  >
-  <button
-      class="btn btn-primary"
-      @click="onSubmit"
-  >
-    Click
-  </button>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit" class="d-flex">
+      <div class="flex-grow-1 m-1">
+        <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button
+            class="btn btn-primary m-1"
+            type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </form>
+    {{ todos }}
+  </div>
 </template>
 
 <script>
@@ -16,14 +28,22 @@
 
   export default {
     setup() {
-      const Name= ref('Kossie');
+      const todo = ref('');
+      const todos = ref([]);
 
-      const onSubmit = () => { // eslint-disable-line no-unused-vars
-        console.log(Name.value);
+      const onSubmit = (e) => { // eslint-disable-line no-unused-vars
+        // submit후 페이지 리로딩 막기
+        // e.preventDefault();
+
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value,
+        });
       };
 
       return {
-        Name,
+        todo,
+        todos,
         onSubmit,
       };
     }
