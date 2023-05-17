bd67f32e24d77ff405e695e39baabed2b01c1bc1
jbb4223 <jbb4223_3top@gsretail.com>
2023-05-17 16:52:57 +0900

    13 class

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit" class="d-flex">
-      <div class="flex-grow-1 m-1">
-        <input
-            class="form-control"
-            type="text"
-            v-model="todo"
-            placeholder="Type new to-do"
-        >
+    <form @submit.prevent="onSubmit" >
+      <div class="d-flex">
+        <div class="flex-grow-1 m-1">
+          <input
+              class="form-control"
+              type="text"
+              v-model="todo"
+              placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button
+              class="btn btn-primary m-1"
+              type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div>
-        <button
-            class="btn btn-primary m-1"
-            type="submit"
-        >
-          Add
-        </button>
+      <div v-show="hasError" style="color : red">
+        This field cannot be empty
       </div>
     </form>
     <div
@@ -35,26 +40,40 @@
 
   export default {
     setup() {
+      const toggle = ref(false);
       const todo = ref('');
       const todos = ref([
         {id: 1, subject: '휴대폰 사기'},
         {id: 2, subject: '장보기'},
       ]);
+      const hasError = ref(false);
 
       const onSubmit = (e) => { // eslint-disable-line no-unused-vars
         // submit후 페이지 리로딩 막기
         // e.preventDefault();
 
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-        });
+        if(todo.value === '') {
+          hasError.value = true;
+        } else {
+          todos.value.push({
+            id: Date.now(),
+            subject: todo.value,
+          });
+          hasError.value = false;
+        }
+      };
+
+      const onToggle = () => {
+        toggle.value = !toggle.value;
       };
 
       return {
+        toggle,
         todo,
         todos,
+        hasError,
         onSubmit,
+        onToggle,
       };
     }
   }
