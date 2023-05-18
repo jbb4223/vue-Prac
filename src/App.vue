9a557a6271fb73214fc25da8259a8db3c7901418
jbb4223 <jbb4223_3top@gsretail.com>
2023-05-18 11:12:07 +0900

    19 class

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,27 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+
     <todo-simple-form @add-todo="addTodo" />
+
     <div v-if="!todos.length">
       추가된 Todo가 없습니다.
     </div>
-    <div
-        v-for="(todo, index) in todos"
-        :key="todo.id"
-        class="card mt-2">
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input class="form-check-input" type="checkbox" v-model="todo.completed">
-          <label class="form-check-label" :class="{ todo: todo.completed }"
-          >
-            {{ todo.subject }}
-          </label>
-        </div>
-        <div>
-          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
+
+    <TodoList :todos="todos" />
   </div>
 </template>
 
 <script>
   import { ref } from 'vue'; // eslint-disable-line no-unused-vars
   import TodoSimpleForm from "./components/TodoSimpleForm"; // eslint-disable-line no-unused-vars
+  import TodoList from "./components/TodoList"; // eslint-disable-line no-unused-vars
 
   export default {
     components: {
       // eslint-disable-next-line vue/no-unused-components
-      TodoSimpleForm
+      TodoSimpleForm,
+      TodoList,
     },
     setup() {
       const toggle = ref(false);
