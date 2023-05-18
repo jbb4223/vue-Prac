62ff526beb78506c9c6a2e6280db0bc1a9c6b63a
jbb4223 <jbb4223_3top@gsretail.com>
2023-05-18 10:39:47 +0900

    17 class

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit" >
-      <div class="d-flex">
-        <div class="flex-grow-1 m-1">
-          <input
-              class="form-control"
-              type="text"
-              v-model="todo"
-              placeholder="Type new to-do"
-          >
-        </div>
-        <div>
-          <button
-              class="btn btn-primary m-1"
-              type="submit"
-          >
-            Add
-          </button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color : red">
-        This field cannot be empty
-      </div>
-    </form>
+    <todo-simple-form />
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
@@ -51,8 +29,13 @@
 
 <script>
   import { ref } from 'vue'; // eslint-disable-line no-unused-vars
+  import TodoSimpleForm from "./components/TodoSimpleForm"; // eslint-disable-line no-unused-vars
 
   export default {
+    components: {
+      // eslint-disable-next-line vue/no-unused-components
+      TodoSimpleForm
+    },
     setup() {
       const toggle = ref(false);
       const todo = ref('');
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="onSubmit" >
+    <div class="d-flex">
+      <div class="flex-grow-1 m-1">
+        <input
+            class="form-control"
+            type="text"
+            v-model="todo"
+            placeholder="Type new to-do"
+        >
+      </div>
+      <div>
+        <button
+            class="btn btn-primary m-1"
+            type="submit"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+    <div v-show="hasError" style="color : red">
+      This field cannot be empty
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    setup () {
+
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style>
+
+</style>
