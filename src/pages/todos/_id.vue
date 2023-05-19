7d903f5b9087b2c65b9584907d38e6d999a90158
jbb4223 <jbb4223_3top@gsretail.com>
2023-05-19 15:44:18 +0900

    48 class

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>Todo Page</div>
+  <h1>To-Do Page</h1>
+  <div v-if="loading"></div>
+  <form v-else>
+    <div class="form-group">
+      <label>Todo subject</label>
+      <input type="text" class="form-control" v-model="todo.subject">
+    </div>
+    <br/>
+    <button class="btn btn-primary">저장</button>
+  </form>
 </template>
 
 <script>
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import axios from 'axios';
 
 export default {
   setup () {
     const route = useRoute();
+    const todo = ref(null);
+    const loading = ref(true);
 
+
+    const getTodo = async () => {
+      const res = await axios.get('http://localhost:3000/todos/' + route.params.id);
+
+      todo.value = res.data;
+      loading.value = false;
+    };
+
+    getTodo();
     console.log(route.params.id);
+
+    return {
+      todo,
+      loading,
+    };
   }
 }
 </script>
